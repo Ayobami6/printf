fbad47a67ea9367da1973d11dcd226909eb2da26
Ayobami <ayobamidele006@gmail.com>
2022-11-19 20:14:13 +0100

    remove the first line

--- print_d-int.c
+++ print_d-int.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
  * printf_int - prints integer
@@ -7,41 +6,41 @@
  */
 int printf_int(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+    int n = va_arg(args, int);
+    int num, last = n % 10, digit, exp = 1;
+    int i = 1;
 
-	n = n / 10;
-	num = n;
+    n = n / 10;
+    num = n;
 
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
+    if (last < 0)
+    {
+        _putchar('-');
+        num = -num;
+        n = -n;
+        last = -last;
+        i++;
+    }
+    if (num > 0)
+    {
+        while (num / 10 != 0)
+        {
+            exp = exp * 10;
+            num = num / 10;
+        }
+        num = n;
+        while (exp > 0)
+        {
+            digit = num / exp;
+            _putchar(digit + '0');
+            num = num - (digit * exp);
+            exp = exp / 10;
+            i++;
+        }
+    }
+    _putchar(last + '0');
 
-	return (i);
+    return (i);
 }
 
 /**
@@ -52,40 +51,40 @@ int printf_int(va_list args)
 
 int printf_dec(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
+    int n = va_arg(args, int);
+    int num, last = n % 10, digit;
+    int i = 1;
+    int exp = 1;
 
-	n = n / 10;
-	num = n;
+    n = n / 10;
+    num = n;
 
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
+    if (last < 0)
+    {
+        _putchar('-');
+        num = -num;
+        n = -n;
+        last = -last;
+        i++;
+    }
+    if (num > 0)
+    {
+        while (num / 10 != 0)
+        {
+            exp = exp * 10;
+            num = num / 10;
+        }
+        num = n;
+        while (exp > 0)
+        {
+            digit = num / exp;
+            _putchar(digit + '0');
+            num = num - (digit * exp);
+            exp = exp / 10;
+            i++;
+        }
+    }
+    _putchar(last + '0');
 
-	return (i);
+    return (i);
 }
