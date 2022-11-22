a0d3f31c3d91351eaf8f4f6bd989a04657aebdd0
ifezue501 <flasysexy501@yahoo.com>
2022-11-22 13:27:42 -0800

    remodified codes and functions

--- main.h
+++ main.h
@@ -1,39 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-
-/* Flag Modifier Macros */
-#define PLUS 1
-#define SPACE 2
-#define HASH 4
-#define ZERO 8
-#define NEG 16
-#define PLUS_FLAG (flags & 1)
-#define SPACE_FLAG ((flags >> 1) & 1)
-#define HASH_FLAG ((flags >> 2) & 1)
-#define ZERO_FLAG ((flags >> 3) & 1)
-#define NEG_FLAG ((flags >> 4) & 1)
 
 /* Length Modifier Macros */
 #define SHORT 1
 #define LONG 2
-
-/**
- * struct buffer_s - A new type defining a buffer struct
- * @buffer: A pointer to a character array
- * @start: A pointer to the start of buffer
- * @len: The length of the string stored in buffer
- */
-typedef struct buffer_s
-{
-	char *buffer;
-	char *start;
-	unsigned int len;
-} buffer_t;
 
 /**
  * struct flags - struct containing flags to "turn on"
@@ -111,12 +84,5 @@ int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char);
-/* Modifiers */
-unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
 
 #endif
